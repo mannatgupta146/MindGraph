6c2e1135f113f57388a02c4943c0eff37b3a5d29
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-05 16:14:14 +0530

    DELETED

--- backend/debug-esm-require.js
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const pdfParse = require('pdf-parse');
-
-console.log('--- DEBUG ESM CREATE-REQUIRE ---');
-console.log('Type of pdfParse:', typeof pdfParse);
-console.log('Is Function?:', typeof pdfParse === 'function');
-console.log('Value:', pdfParse);
-if (pdfParse.default) console.log('Type of .default:', typeof pdfParse.default);
